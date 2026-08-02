318e2d836121c01c829460532d05b70afdff7652
Adolphe <adolphechristopher@gmail.com>
2026-08-02 08:39:44 +0200

    fix(build): correct relative paths for shared UI components

--- apps/web/pages/formations/[id].tsx
+++ apps/web/pages/formations/[id].tsx
@@ -1,5 +1,5 @@
-import Header from '../../shared/src/Header';
-import Footer from '../../shared/src/Footer';
+import Header from '../../../shared/src/Header';
+import Footer from '../../../shared/src/Footer';
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
